6aac3f2e6aa715b657dc4518a16bdb368b495030
hyunyyxp <qqqq_o@naver.com>
2024-02-02 11:59:21 +0900

    참조 데이터 타입

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
-const age = 20;
-const job = '개발자';
+const job = {
+    developer : '개발자'
+}
 
-const msg = `저는 ${job}이고, ${age}살 입니다.`;
-console.log(msg);
+console.log(job.developer);
+
+const hyun = {
+    age : 27,
+    job : '개발자',
+    baking : {
+        saltBread : 3,
+        cake : 1
+    }
+}
+
+console.log(hyun);
+console.log(hyun.baking);
