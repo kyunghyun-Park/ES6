669840c8614637cf658d8a45e7d574261c017427
hyunyyxp <qqqq_o@naver.com>
2024-02-02 14:58:48 +0900

    문자열 method

--- index.js
+++ index.js
@@ -1,17 +1,6 @@
+const str = 'Hello';
 
-//default parameter
-function sum(a, b = 0) {
-    console.log(a + b);
-}
-
-sum(10);
-sum(10, 20);
-
-//rest parameter
-function print(a, b, ...rest) {
-    console.log(a);
-    console.log(b);
-    console.log(rest);
-}
-
-print(10, 20, 30, 40, 50, 60, 70);
+console.log(str.repeat(3)); //HelloHelloHello
+console.log(str.padStart(10, '_')) //_____Hello
+console.log(str.padEnd(10, '_')) //Hello_____
+console.log(str.includes('He')) //true
